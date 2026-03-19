4d4fcb86f2a13e833ddbafa3a97bb093d160b1e5
IGOR EUGENIO GOMES PACHECO PEREIRA <igor.pereira3@alunos>
2026-03-19 19:52:54 -0300

    So começa

---
+++ src/main/java/com/senac/br/Prova1/Prova1ApiApplication.java
@@ -0,0 +1,13 @@
+package com.senac.br.Prova1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova1ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova1ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/br/Prova1/Prova1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.br.Prova1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova1ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
